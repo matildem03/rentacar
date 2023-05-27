daecdf9294387d56cb40b63b98e370907a9e1a0f
matildem03 <matildesmarques2003@gmail.com>
2023-05-27 18:48:40 +0100

    changes

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
-function showContent() {
-    var radio = document.getElementById("paga-mul");
-    var content = document.getElementById("content");
+function showElement() {
+    var radio = document.getElementById("multibanco");
+    var content = document.getElementById("hiddenelement");
 
     if (radio.checked) {
-        content.styles.display = "block";
+        content.style.display = "block";
     } else {
-        content.styles.display = "none";
+        content.style.display = "none";
+    }
+}
+
+function hideElement() {
+    var radio = document.getElementById("paypal");
+    var content = document.getElementById("hiddenelement");
+
+    if (radio.checked) {
+        content.style.display = "none";
+    } else {
+        content.style.display = "none";
+    }
+}
+
+function hideElement1() {
+    var radio = document.getElementById("mbway");
+    var content = document.getElementById("hiddenelement");
+
+    if (radio.checked) {
+        content.style.display = "none";
+    } else {
+        content.style.display = "none";
+    }
+}
+
+function hideElement() {
+    var radio = document.getElementById("multibanco");
+    var content = document.getElementById("buttonesconde");
+
+    if (radio.checked) {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
     }
 }
